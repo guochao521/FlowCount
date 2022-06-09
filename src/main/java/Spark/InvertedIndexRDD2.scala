ac4a04441d47e009f56b157a2bd47bcef0b4cfb4
wangguochao <wangguochao@sinovoice.com.cn>
2022-06-09 16:08:22 +0800

    添加spark

--- src/main/java/Spark/InvertedIndexRDD2.scala
+++ src/main/java/Spark/InvertedIndexRDD2.scala
@@ -15,7 +15,7 @@ object InvertedIndexRDD2 {
 
     val sc = new SparkContext(sparkConf)
 
-    sc.setLogLevel("INFO")
+    sc.setLogLevel("WARN")
 
     // 1. 获取hadoop操作文件的api
     val fs = FileSystem.get(sc.hadoopConfiguration)
@@ -37,14 +37,20 @@ object InvertedIndexRDD2 {
 
     // 5. 构建词频（（文件名，单词），词频）
     val wordRDD = unionRDD.map(word => {(word, 1)}).reduceByKey(_ + _)
+    println("*************词频RDD*******************")
     wordRDD.foreach(println)
 
     // 6. 调整数据格式，将（（文件名，单词），词频）=> （单词，（文件名，词频）） => （单词，（文件名，词频）） 汇总输出
     val formatRDD1 = wordRDD.map(word => {(word._1._2, String.format("(%s,%s)", word._1._1, word._2.toString))})
+
+    println("************调整数据格式 阶段1***********")
     formatRDD1.foreach(println)
+
     val formatRDD2 = formatRDD1.reduceByKey(_ + "," + _)
+    println("************调整数据格式 阶段2***********")
     formatRDD2.foreach(println)
     val formatRDD3 = formatRDD2.map(word => String.format("\"%s\", {%s}", word._1, word._2))
+    println("************调整数据格式 阶段3 (最后输出结果)（***********")
     formatRDD3.foreach(println)
 
     formatRDD3.repartition(1).saveAsTextFile(output)
